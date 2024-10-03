be68531a734e39d25f46a006b5905b5d7ba408fb
harshini-ns <harshininaveen9801@gmail.com>
2024-10-03 13:24:56 +0530

    for vercel

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 //  web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBIpafFeAfqI-tugOJIKdDr_MNNGDBGbwU",
-    authDomain: "daysinn-6727d.firebaseapp.com",
-    projectId: "daysinn-6727d",
-    storageBucket: "daysinn-6727d.appspot.com",
-    messagingSenderId: "750057623349",
-    appId: "1:750057623349:web:e838e134b2d92164e5867c",
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
